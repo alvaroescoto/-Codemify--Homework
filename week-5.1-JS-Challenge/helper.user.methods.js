10ab31ec11462fe0e1b1dd9ffc2358a658eece04
alvaroescoto <alvaroedeavila@gmail.com>
2024-11-06 09:24:22 +0100

    5.1 challenge

--- week-5.1-JS-Challenge/helper.user.methods.js
+++ week-5.1-JS-Challenge/helper.user.methods.js
@@ -1,35 +1,65 @@
 class Helpers {
-    getUsersByStatus(response, status) {
-        const users = [];
+  getUsersByStatus(response, status) {
+    const users = [];
 
-        
+    for (let i = 0; i < response.users.length; i++) {
+      const element = response.users[i];
+      const elementStatus = element.role.status;
+      const elementDescription = element.role.description;
 
-        return users;
+      if (
+        elementStatus === status &&
+        elementDescription != null &&
+        elementDescription != ""
+      ) {
+        users.push(element.name);
+      }
     }
 
-    getUsersByRole(response, role, roleId) {
-        const users = [];
+    return users;
+  }
 
-        // Your code here
+  getUsersByRole(response, role, roleId) {
+    const users = [];
 
-        return users;
+    for (let i = 0; i < response.users.length; i++) {
+      const element = response.users[i];
+      const elementRole = element.role.name;
+      const elementRoleId = element.role.id;
+      if (elementRole === role && elementRoleId === roleId) {
+        users.push(element.name);
+      }
+    }
+    return users;
+  }
+  getUsersByEmailDomain(response, domain) {
+    const users = [];
+
+    for (let i = 0; i < response.users.length; i++) {
+      const element = response.users[i];
+      const email = element.email;
+      if (email.includes("codemify")) {
+        users.push(element.name);
+      }
     }
 
-    getUsersByEmailDomain(response, domain) {
-        const users = [];
+    return users;
+  }
 
-        // Your code here
+  getUserBalanceByCurrency(response, currency) {
+    const balances = [];
 
-        return users;
+    for (let i = 0; i < response.users.length; i++) {
+      const element = response.users[i];
+      const elementBalance = element.balance;
+      const elementCurrency = element.currency;
+      if (elementCurrency === currency) {
+        balances.push(element.balance);
+      }
     }
 
-    getUserBalanceByCurrency(response, currency) {
-        const balances = [];
-
-        // Your code here
-
-        return balances;
-    }
+    return balances;
+  }
 }
 
-export default new Helpers()
+export default new Helpers();
