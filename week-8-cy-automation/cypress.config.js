bb1d59eed63ea083a7a04780e7bc6292950e9c3e
alvaroescoto <alvaroedeavila@gmail.com>
2024-12-19 05:58:27 +0100

    updates week 8

--- week-8-cy-automation/cypress.config.js
+++ week-8-cy-automation/cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "https://dev.delekhomes.com/",
+    viewportWidth: 1400,
+    viewportHeight: 1200,
     },
-  },
-});
+  })
+
