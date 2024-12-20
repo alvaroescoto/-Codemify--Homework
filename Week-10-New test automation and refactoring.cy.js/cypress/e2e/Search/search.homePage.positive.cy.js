518445a2a98073f41da763ac55f6c589c3718610
alvaroescoto <alvaroedeavila@gmail.com>
2024-12-20 16:05:00 +0100

    week 10

---
+++ Week-10-New test automation and refactoring.cy.js/cypress/e2e/Search/search.homePage.positive.cy.js
@@ -0,0 +1,45 @@
+import houseInfo from "../../fixtures/testData/listingsDetails.json"
+import homePage from "../../page_objects/home.page";
+import feacturedListingPage from "../../page_objects/feacturedListing.page";
+
+describe("Search HomePage Tests", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    homePage.switchLightBtn.click();
+  });
+
+  it("Should search by keyword", () => {
+    homePage.searchInput.first().type("scary");
+    homePage.searchBtn.click();
+    cy.contains(houseInfo.searchResult.houseName);
+  });
+
+  it("Should search by bedrooms", () => {
+    homePage.bedroomsBtn.click();
+    homePage.bedroomsInput.click();
+    homePage.searchBtn.click();
+    feacturedListingPage.moreInfoBtn.click();
+    feacturedListingPage.bedroomsLoc.should('be.at.least', 2)
+    });
+      
+  it("Should search by city", () => {
+    homePage.cityInput.type(houseInfo.searchResult.city);
+    homePage.searchBtn.click();
+    feacturedListingPage.cityUniqueLoc
+    .filter(':contains("City: Rivera")')
+    .should("have.length", 1);
+    houseInfo.searchResult.houseDetails.forEach((text) => {
+    feacturedListingPage.houseInfo.contains(text).should("be.visible");
+    });
+    feacturedListingPage.moreInfoBtn.click();
+    houseInfo.moreInfo.forEach((text) => {
+      feacturedListingPage.moreInfoDetail.contains(text).should("be.visible");
+    });
+  });
+
+  it("Should search by price", () => {
+    cy.visit("/featured-listings?price=1000000-1000000");
+    cy.contains(houseInfo.searchResult.houseName);
+    feacturedListingPage.housePriceLoc.should('contain.text', '$ 1,000,000');
+  });
+});
