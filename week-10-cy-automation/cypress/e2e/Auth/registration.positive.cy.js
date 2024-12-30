35e811e602ad11cc670382843d97317698ec42dd
alvaroescoto <alvaroedeavila@gmail.com>
2024-12-30 11:43:22 +0100

    working

---
+++ week-10-cy-automation/cypress/e2e/Auth/registration.positive.cy.js
@@ -0,0 +1,31 @@
+import userCredentials from '../../fixtures/testData/User.Credentials.Fixture.File.json'
+import { fakerDE as faker } from "@faker-js/faker";
+import homePage from "../../page_objects/home.page";
+import registrationPage from "../../page_objects/registration.page";
+import dashboardPage from '../../page_objects/dashboard.page';
+
+const fakeUser = {
+  lastName: faker.name.lastName(), 
+  firstName: faker.name.firstName(), 
+  email: faker.internet.email(),
+  password: faker.internet.password()
+};
+
+describe("Registration Positive", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Should register new account", () => {
+    homePage.registrationBtn.click();
+    registrationPage.registration(
+      fakeUser.firstName,
+      fakeUser.lastName,
+      fakeUser.email,
+      fakeUser.password
+    );
+    registrationPage.registrationButton;
+    dashboardPage.getFakeUser(fakeUser.firstName, fakeUser.lastName).should("be.visible")
+    dashboardPage.userRoleLoc.should("have.text", userCredentials.usersRole.user);
+  });
+});
---
+++ week-10-cy-automation/cypress/e2e/Search/search.listingPage.positive.cy.js
@@ -0,0 +1,51 @@
+import homePage from "../../page_objects/home.page";
+import feacturedListingPage from "../../page_objects/feacturedListing.page";
+import listingDetails from "../../fixtures/testData/listingsDetailsUi.json";
+
+let houseId;
+
+describe("Search thruogh Feactured Listings Page", () => {
+  before(() => {
+    cy.login();
+    cy.createLisitng().then((id) => {houseId = id;});
+  });
+
+  beforeEach(() => {
+    cy.visit("/featured-listings");
+    homePage.switchLightBtn.click();
+  });
+
+  after(() => {
+    cy.deleteLisitngById(houseId);
+  });
+
+  it("Should search by keyword", () => {
+    feacturedListingPage.searchInput.type(listingDetails.newListingPage.description);
+    feacturedListingPage.searchBtn.click();
+    cy.contains(listingDetails.newListingPage.houseName);
+  });
+
+  it("Should search by bedrooms", () => {
+    feacturedListingPage.bedroomsBtn.click();
+    feacturedListingPage.bedroomsInput.click();
+    feacturedListingPage.searchBtn.click();
+    feacturedListingPage.moreInfoBtn.click();
+    feacturedListingPage.bedroomsLoc.should("be.at.least", 2);
+  });
+
+  it("Should search by city", () => {
+    feacturedListingPage.cityInput.type(listingDetails.newListingPage.city);
+    feacturedListingPage.searchBtn.click();
+    feacturedListingPage.cityUniqueLoc.should("have.length", 1);
+    feacturedListingPage.moreInfoBtn.click();
+    listingDetails.newListingDetails.forEach((text) => 
+      {feacturedListingPage.moreInfoDetail.contains(text).should('be.visible')});
+  });
+
+  it("Should search by price", () => {
+    cy.visit("/featured-listings?price=6000000-8000000");
+    feacturedListingPage.housePriceLoc.invoke("text")
+      .should("include", "$ 7,000,000")
+      .and("include", listingDetails.newListingPage.houseName);
+  });
+});
